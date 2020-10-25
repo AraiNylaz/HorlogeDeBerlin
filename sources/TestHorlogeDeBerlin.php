d5d7a1c15121112df01407bcbcda9a15ee15a80f
AraiNylaz <lorine.etienne@gmail.com>
2020-10-25 15:22:17 +0100

    add method "5 by 5" for input 35

--- sources/TestHorlogeDeBerlin.php
+++ sources/TestHorlogeDeBerlin.php
@@ -175,4 +175,13 @@ class TestHorlogeDeBerlin extends TestCase
         //assert
         $this->assertEquals("[5min][10min][15MIN][20min][25min][30MIN]",$actual);
     }
+    public function test_display_given35_shouldReturn7()
+    {
+        //arrange
+
+        //act
+        $actual = $this->horlogeDeBerlin->display_5_by_5_minutes(35);
+        //assert
+        $this->assertEquals("[5min][10min][15MIN][20min][25min][30MIN][35min]",$actual);
+    }
 }
