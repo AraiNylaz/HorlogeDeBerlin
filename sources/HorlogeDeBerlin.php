4f517c940b0b80ee8feffb968f0c71c3e78bb73a
user <user@PC-1P0-048.OASIS.UCLOUVAIN.BE>
2020-10-25 10:59:58 +0100

    add first test unit and function

--- sources/HorlogeDeBerlin.php
+++ sources/HorlogeDeBerlin.php
@@ -1,1 +1,25 @@
 <?php
+date_default_timezone_set('Europe/Berlin');
+$time = date("H:i:s");
+$date = new \DateTime();
+$hours = $date->format('H');
+$minutes = $date->format('i');
+$seconds = $date->format('s');
+
+$minuteModulo5 = $minutes%5;
+$minuteDividedBy5 = $minutes/5;
+
+$hoursModulo5 = $hours%5;
+$hourDividedBy5 = $hours/5;
+
+class HorlogeDeBerlin{
+
+    public $arraySimpleMinutes = array("1st Minute","2sd Minute","3rd Minute","4th Minute");
+    public $array5Minutes = array("5min", "10min", "15MIN", "20min", "25min", "30MIN", "35min", "40min", "45MIN", "50min", "55min");
+    public function __construct(){}
+
+    public function display_simple_minutes($minute): string{
+        if($minute === 1) return "[".$this->arraySimpleMinutes[0]."]";
+
+    }
+}
--- sources/TestHorlogeDeBerlin.php
+++ sources/TestHorlogeDeBerlin.php
@@ -1,1 +1,28 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: lorine
+ * Date: 13-10-20
+ * Time: 18:11
+ */
+require "HorlogeDeBerlin.php";
+use PHPUnit\Framework\TestCase;
+
+class TestHorlogeDeBerlin extends TestCase
+{
+    private $horlogeDeBerlin;
+    public function setUp(): void{
+        parent::setUp();
+        $this->horlogeDeBerlin = new HorlogeDeBerlin();
+    }
+
+    public function test_display_given1_shouldReturn1()
+    {
+        //arrange
+
+        //act
+        $actual = $this->horlogeDeBerlin->display_simple_minutes(1);
+        //assert
+        $this->assertEquals("[1st Minute]",$actual);
+    }
+}
